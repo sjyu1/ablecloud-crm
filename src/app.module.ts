2dbd2f47de0605af4d166da18751793507889133
jeong da-young <bja05008@gmail.com>
2025-02-27 16:39:19 +0900

    디비 주소 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,16 @@ import { LicenseModule } from './license/license.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '10.10.254.24',
+      host: '10.10.254.208',
       port: 3306,
-      username: 'nestjs_user',
+      username: 'user',
       password: 'Ablecloud1!',
-      database: 'nestjs_db12',
+      database: 'licenses',
       entities: [__dirname + '/**/*.entity.{js,ts}'],
       synchronize: true,
+      extra: {
+        allowPublicKeyRetrieval: true,
+      },
     }),
     LicenseModule,
   ],
