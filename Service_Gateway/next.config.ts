6659ff041a4e2ae74f83d168501d728934e2d7a2
sjyu1 <93505580+sjyu1@users.noreply.github.com>
2025-04-04 13:01:11 +0900

    Update next.config.ts

--- Service_Gateway/next.config.ts
+++ Service_Gateway/next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  devIndicators: false
+  devIndicators: {
+    appIsrStatus: false,
+  },
 };
 
 export default nextConfig;
