6af43b2da2595d435c692bf31a5be12859e24502
sjyu1 <93505580+sjyu1@users.noreply.github.com>
2025-05-07 09:51:15 +0900

    로그파일 경로 변경

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -11,7 +11,7 @@ mkdir -p $BACKUP_DIR
 mysqldump --user=$DB_USER --password=$DB_PASS $DB_NAME > $BACKUP_DIR/$DB_NAME-$DATE.sql
 gzip $BACKUP_DIR/$DB_NAME-$DATE.sql
 
-find $BACKUP_DIR -type f -name "*.gz" -mtime +7 -delete
+find $BACKUP_DIR -type f -name "*.gz" -mtime +2 -delete
 
 
 
