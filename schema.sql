2e9e312a3fe141e591d19e40ecf8b6351d86525e
sjyu1 <93505580+sjyu1@users.noreply.github.com>
2026-04-30 14:16:41 +0900

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -16,6 +16,32 @@ CREATE TABLE `business` (
   `updated` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `removed` datetime(6) DEFAULT NULL,
   `details` text,
+  `deposit_use` tinyint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- business_product_ver
+CREATE TABLE `business_product_ver` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `business_id` int NOT NULL,
+  `version` varchar(2555) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `note` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `created` datetime(6) DEFAULT CURRENT_TIMESTAMP(6),
+  `removed` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- credit
+CREATE TABLE `credit` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `partner_id` int DEFAULT NULL,
+  `business_id` int DEFAULT NULL,
+  `deposit` int DEFAULT NULL,
+  `credit` int DEFAULT NULL,
+  `note` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `created` datetime(6) DEFAULT CURRENT_TIMESTAMP(6),
+  `updated` datetime(6) DEFAULT CURRENT_TIMESTAMP(6),
+  `removed` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -28,6 +54,7 @@ CREATE TABLE `customer` (
   `manager_id` varchar(255) NOT NULL DEFAULT '',
   `updated` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `removed` datetime(6) DEFAULT NULL,
+  `manager_company_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -41,6 +68,7 @@ CREATE TABLE `license` (
   `issued` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `expired` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `trial` tinyint(1) DEFAULT NULL,
+  `oem` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `business_id` int DEFAULT NULL,
   `approve_user` varchar(50) DEFAULT NULL,
   `approved` datetime(6) DEFAULT NULL,
@@ -51,12 +79,26 @@ CREATE TABLE `license` (
   UNIQUE KEY `IDX_1452e56036f836c55bd48ba298` (`license_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- notice
+CREATE TABLE `notice` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `writer` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `level` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `created` datetime(6) DEFAULT CURRENT_TIMESTAMP(6),
+  `updated` datetime(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `removed` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- partner
 CREATE TABLE `partner` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `level` enum('PLATINUM','GOLD','SILVER','VAR') NOT NULL DEFAULT 'GOLD',
   `telnum` varchar(255) NOT NULL,
+  `product_category` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `created` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `updated` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `removed` datetime(6) DEFAULT NULL,
@@ -66,11 +108,48 @@ CREATE TABLE `partner` (
 -- product
 CREATE TABLE `product` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `category_id` int DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `version` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `isoFilePath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `checksum` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `enabled` tinyint(1) DEFAULT NULL,
   `contents` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `created` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `removed` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- product_category
+CREATE TABLE `product_category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `enabled` tinyint(1) DEFAULT '1',
+  `created` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `removed` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- support
+CREATE TABLE `support` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int DEFAULT NULL,
+  `business_id` int DEFAULT NULL,
+  `issued` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `type` enum('poc','consult','technical','other','incident') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'consult' COMMENT '''poc:PoC'',''consult:'',''technical:'',''other:'',''incident:''',
+  `issue` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `solution` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `actioned` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `action_type` enum('mail','remote','phone','site') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'phone' COMMENT '''mail:'',''remote:'',''phone:,site:''',
+  `manager` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `status` enum('processing','complete') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'processing' COMMENT '''processing:'',''complete:''',
+  `requester` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `requester_telnum` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `requester_email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `writer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `note` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `created` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `updated` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `removed` datetime(6) DEFAULT NULL,
