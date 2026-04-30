c0d1124fc79f5f4e8a878da7390719df1d15af8f
sjyu1 <93505580+sjyu1@users.noreply.github.com>
2026-04-30 14:16:39 +0900

    Update mysql_backup.sh

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -3,18 +3,22 @@
 DB_USER="user"
 DB_PASS="Ablecloud1!"
 DB_NAME="licenses"
-BACKUP_DIR="/root/mysql_backup"
+DB_USER_NAME="keycloak"
+BACKUP_DIR="/opt/mysql_backup"
 DATE=$(date +"%Y-%m-%d")
 
 mkdir -p $BACKUP_DIR
 
-mysqldump --user=$DB_USER --password=$DB_PASS $DB_NAME > $BACKUP_DIR/$DB_NAME-$DATE.sql
+mysqldump --user=$DB_USER --password=$DB_PASS $DB_NAME > $BACKUP_DIR/$DB_NAME-$DATE.sql --single-transaction --quick --no-tablespaces
 gzip $BACKUP_DIR/$DB_NAME-$DATE.sql
 
-find $BACKUP_DIR -type f -name "*.gz" -mtime +2 -delete
+mysqldump --user=$DB_USER --password=$DB_PASS $DB_USER_NAME > $BACKUP_DIR/$DB_USER_NAME-$DATE.sql --single-transaction --quick --no-tablespaces
+gzip $BACKUP_DIR/$DB_USER_NAME-$DATE.sql
+
+find $BACKUP_DIR -type f -name "*.gz" -mtime +7 -delete
 
 
 
 # crontab(매일 1시마다 백업. backup_script.sh 파일권한 확인)
 # crontab -e
-# 0 1 * * * /root/ablecloud-crm/backup_script.sh >> /root/ablecloud-crm/backup_script.sh.log 2>&1
+# 0 1 * * * /opt/mysql_backup.sh >> /opt/mysql_backup.log 2>&1
